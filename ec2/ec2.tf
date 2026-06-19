03855b6be44839ec751c73d81142a4aaf47b3bbe
Neelisetty Kesava Tarun Kumar <2200030017cse@gmail.com>
2026-06-19 17:56:55 +0530

    actions

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
-  }
-  
+    Name = "terraform"
+    Project = "roboshop"
+  } 
 }
